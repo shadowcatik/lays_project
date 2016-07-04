22f2d3f86fd4fad2b5195412cf09786d301e5cf7
Oksana Melnik <kyoshiniimura@gmail.com>
2016-07-04 14:05:15 +0300

    Add project

---
+++ app/js/common.js
@@ -0,0 +1,43 @@
+$(function(){
+
+	//svg inject plugin
+
+	$('[data-js-svg]').svgInject(function() {
+	    // Injection complete
+	});
+
+	//canvas
+
+	window.onload = function() {
+	    var drawingCanvas = document.getElementById('instagram');
+	    if(drawingCanvas && drawingCanvas.getContext) {
+		    var context = drawingCanvas.getContext('2d');
+		     // Внешняя окружность 
+		    context.strokeStyle = "#f73934";
+		    context.fillStyle = "#f73934";
+		    context.shadowColor = '#f73934';
+		    context.shadowBlur = 40;
+		    context.beginPath();
+		    context.arc(200,100,5,0,Math.PI*2,true);
+		    context.closePath();
+		    context.stroke();
+		    context.fill();
+		    //Внутренняя окружность
+		    context.fillStyle = "#ff0000";
+		    context.beginPath();
+		    context.arc(100,100,1,0,Math.PI*2,true);
+		    context.closePath();
+		    context.stroke();
+		    context.fill();
+			//изображение
+		    cellSize = 8,
+		    pic = new Image(),
+		    instagram.width = 8 * cellSize;
+			instagram.height = 8 * cellSize;
+			pic.src = '../img/instagram_color.png';
+			pic.onload = function () {
+			    context.drawImage(pic, 0, 0)
+		    }
+	    }
+    }
+});

f9907a61ef2ed81ace6fe41f06fa4dbb1f977eec
OksanaMelnik <kyoshiniimura@gmail.com>
2016-07-18 00:38:46 +0300

    Build project

--- dist/js/common.js
+++ dist/js/common.js
@@ -1,3 +1,48 @@
 $(function(){
-	$('body').fadeOut();
+
+	//svg inject plugin
+
+	$('[data-js-svg]').svgInject(function() {
+	    // Injection complete
+	});
+
+    //mobile menu
+	
+	$(document).ready(function() {
+	    $('#show-menu').click(function() {
+	      $('.menu, .icon--close').fadeIn();
+	      $('.icon--menu').fadeOut();
+	      return false;
+	    });
+	    $('.close').click(function() {
+	      $('.menu, .icon--close').fadeOut();
+	      $('.icon--menu').fadeIn();
+	      return false;
+	    });
+	});
+
+	//Visible for animation
+	$(this).scroll(function(){
+    function isVisible(elem) {
+    
+      var coords = elem.getBoundingClientRect();
+    
+      var windowHeight = document.documentElement.clientHeight;
+    
+      var topVisible = coords.top > 0 && coords.top < windowHeight;
+      var bottomVisible = coords.bottom < windowHeight && coords.bottom > 0;
+    
+      return topVisible || bottomVisible;
+    }
+    $('[data-js-animation]').each(function(index, elem){
+      if(isVisible(elem)){
+        $(elem).hasClass("left") && $(elem).addClass("animated--right");
+        $(elem).hasClass("right") && $(elem).addClass("animated--left");
+      }
+      else{
+        $(elem).removeClass("animated--right animated--left");
+      }
+    });
+  });
+
 });
